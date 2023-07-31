d66376038a58b6b8d847c6b66a5239f9a4237fb9
dev-marianacavalcante <mariana.cavalcantevl@gmail.com>
2023-07-31 16:58:27 -0300

    Confirm deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://dev-marianacavalcante.github.io",
+  base: "https://dev-marianacavalcante.github.io/GithubBlog",
+  mode: "production",
+  appType: "spa",
+  esbuild: {
+    jsxFactory: "React.createElement",
+    jsxFragment: "React.Fragment",
+  },
 });
